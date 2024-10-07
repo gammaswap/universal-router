38777dde8369bc368cc6bc1f3ff321edcfa824fe
Daniel Alcarraz <dam417@nyu.edu>
2024-10-07 04:35:20 -0400

    added more logic for aerodrome

--- contracts/UniversalRouter.sol
+++ contracts/UniversalRouter.sol
@@ -43,6 +43,9 @@ contract UniversalRouter {
         } else if(protocolId == 4) { // Aerodrome Non Stable
             // TODO: need the hashcode, it's the IPoolFactory(factory).implementation() in Aerodrome's github
             return hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
+        } else if(protocolId == 5) { // Aerodrome Stable
+            // TODO: need the hashcode, it's the IPoolFactory(factory).implementation() in Aerodrome's github
+            return hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
         }
         return hex'00';
     }
@@ -52,6 +55,8 @@ contract UniversalRouter {
             return keccak256(abi.encodePacked(token0, token1));
         } else if(protocolId == 4) {
             return keccak256(abi.encodePacked(token0, token1, false));
+        } else if(protocolId == 5) {
+            return keccak256(abi.encodePacked(token0, token1, true));
         }
         return hex'00';
     }
@@ -116,10 +121,40 @@ contract UniversalRouter {
             if(protocolId == 3) {
                 fee = calcPairTradingFee(amountIn, reserveIn, reserveOut, pair);
             }
+            amountOut = getAmountOut(amountIn, reserveIn, reserveOut, fee);
+        } else if(protocolId == 4 || protocolId == 5) {
+            (pair,,) = pairFor(factory, tokenA, tokenB, protocolId);
+            //amountOut = IPool(pair).getAmountOut(amountIn, tokenA); TODO: This is supposed to be the AeroDrome IPool interface
         }
-        amountOut = getAmountOut(amountIn, reserveIn, reserveOut, fee);
 
         require(amountOut > 0, "ZERO_AMOUNT");
+    }
+
+    // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
+    function quotePath(uint256 amountIn, bytes memory path) internal view returns (uint256 amountOut, uint256[] memory reserves) {
+        require(path.length >= 45 && (path.length - 20) % 25 == 0, "INVALID_PATH");
+        reserves = new uint256[](path.numPools() * 2);
+        amountOut = amountIn;
+        uint256 i = 0;
+        while (true) {
+            bool hasMultiplePools = path.hasMultiplePools();
+            // only the first pool in the path is necessary
+            (address tokenA, address tokenB, uint16 protocolId, uint24 fee) = path.getFirstPool().decodeFirstPool();
+
+            (reserves[i], reserves[i + 1],) = getReserves(factory, tokenA, tokenB, protocolId);
+
+            amountOut = quote(amountOut, reserves[i], reserves[i + 1]);
+
+            // decide whether to continue or terminate
+            if (hasMultiplePools) {
+                path = path.skipToken();
+            } else {
+                break;
+            }
+            unchecked {
+                i += 2;
+            }
+        }
     }
 
     function getAmountsOut(uint256 amountIn, bytes memory path) public view virtual returns (uint256[] memory amounts) {
@@ -166,6 +201,10 @@ contract UniversalRouter {
                 fee = 3;
                 amountIn = getAmountIn(amountOut, reserveIn, reserveOut, fee);
             }
+        } else if(protocolId == 4 || protocolId == 5) {
+            // TODO: Need to get parameters for AeroDrome function call and update logic for getAmountsIn
+            // We could also update logic to function for getAmountsOut to avoid making the external call
+            //amountIn = getAmountOutAerodrome();
         }
 
         require(amountIn > 0, "ZERO_AMOUNT");
@@ -195,6 +234,135 @@ contract UniversalRouter {
                 --i;
             }
         }
+    }
+
+    function getAmountOutAerodrome(uint256 amountIn, address tokenIn, address token0, uint256 reserve0, uint256 reserve1, uint256 decimals0, uint256 decimals1, bool stable) external view returns (uint256) {
+        (uint256 _reserve0, uint256 _reserve1) = (reserve0, reserve1);
+        //TODO: need to add IPoolFactory interface
+        //amountIn -= (amountIn * IPoolFactory(factory).getFee(address(this), stable)) / 10000; // remove fee from amount received
+        if(stable) {
+            return _getAmountOutStable(amountIn, tokenIn, token0, _reserve0, _reserve1, decimals0, decimals1);
+        } else {
+            return _getAmountOutNonStable(amountIn, tokenIn, token0, _reserve0, _reserve1);
+        }
+    }
+
+    function _getAmountOutStable(
+        uint256 amountIn,
+        address tokenIn,
+        address token0,
+        uint256 _reserve0,
+        uint256 _reserve1,
+        uint256 decimals0,
+        uint256 decimals1
+    ) internal view returns (uint256) {
+        uint256 xy = _k(_reserve0, _reserve1, decimals0, decimals1, true);
+        _reserve0 = (_reserve0 * 1e18) / decimals0;
+        _reserve1 = (_reserve1 * 1e18) / decimals1;
+        (uint256 reserveA, uint256 reserveB) = tokenIn == token0 ? (_reserve0, _reserve1) : (_reserve1, _reserve0);
+        amountIn = tokenIn == token0 ? (amountIn * 1e18) / decimals0 : (amountIn * 1e18) / decimals1;
+        uint256 y = reserveB - _get_y(amountIn + reserveA, xy, reserveB, decimals0, decimals1);
+        return (y * (tokenIn == token0 ? decimals1 : decimals0)) / 1e18;
+    }
+
+    function _getAmountOutNonStable(
+        uint256 amountIn,
+        address tokenIn,
+        address token0,
+        uint256 _reserve0,
+        uint256 _reserve1
+    ) internal view returns (uint256) {
+        (uint256 reserveA, uint256 reserveB) = tokenIn == token0 ? (_reserve0, _reserve1) : (_reserve1, _reserve0);
+        return (amountIn * reserveB) / (reserveA + amountIn);
+    }
+
+    function _f(uint256 x0, uint256 y) internal pure returns (uint256) {
+        uint256 _a = (x0 * y) / 1e18;
+        uint256 _b = ((x0 * x0) / 1e18 + (y * y) / 1e18);
+        return (_a * _b) / 1e18;
+    }
+
+    function _d(uint256 x0, uint256 y) internal pure returns (uint256) {
+        return (3 * x0 * ((y * y) / 1e18)) / 1e18 + ((((x0 * x0) / 1e18) * x0) / 1e18);
+    }
+
+    function _get_y(uint256 x0, uint256 xy, uint256 y, uint256 decimals0, uint256 decimals1) internal view returns (uint256) {
+        for (uint256 i = 0; i < 255; i++) {
+            uint256 k = _f(x0, y);
+            if (k < xy) {
+                // there are two cases where dy == 0
+                // case 1: The y is converged and we find the correct answer
+                // case 2: _d(x0, y) is too large compare to (xy - k) and the rounding error
+                //         screwed us.
+                //         In this case, we need to increase y by 1
+                uint256 dy = ((xy - k) * 1e18) / _d(x0, y);
+                if (dy == 0) {
+                    if (k == xy) {
+                        // We found the correct answer. Return y
+                        return y;
+                    }
+                    if (_k(x0, y + 1, decimals0, decimals1, true) > xy) {
+                        // If _k(x0, y + 1) > xy, then we are close to the correct answer.
+                        // There's no closer answer than y + 1
+                        return y + 1;
+                    }
+                    dy = 1;
+                }
+                y = y + dy;
+            } else {
+                uint256 dy = ((k - xy) * 1e18) / _d(x0, y);
+                if (dy == 0) {
+                    if (k == xy || _f(x0, y - 1) < xy) {
+                        // Likewise, if k == xy, we found the correct answer.
+                        // If _f(x0, y - 1) < xy, then we are close to the correct answer.
+                        // There's no closer answer than "y"
+                        // It's worth mentioning that we need to find y where f(x0, y) >= xy
+                        // As a result, we can't return y - 1 even it's closer to the correct answer
+                        return y;
+                    }
+                    dy = 1;
+                }
+                y = y - dy;
+            }
+        }
+        revert("!y");
+    }
+
+    function _k(uint256 x, uint256 y, uint256 decimals0, uint256 decimals1, bool stable) internal view returns (uint256) {
+        if (stable) {
+            uint256 _x = (x * 1e18) / decimals0;
+            uint256 _y = (y * 1e18) / decimals1;
+            uint256 _a = (_x * _y) / 1e18;
+            uint256 _b = ((_x * _x) / 1e18 + (_y * _y) / 1e18);
+            return (_a * _b) / 1e18; // x3y+y3x >= k
+        } else {
+            return x * y; // xy >= k
+        }
+    }
+
+    /// TODO: Must incorporate this logic in the quoting logic for AeroDrome stable pools
+    function quoteStableLiquidityRatio(
+        address tokenA,
+        address tokenB,
+        address _factory
+    ) external view returns (uint256 ratio) {
+        /*IPool pool = IPool(poolFor(tokenA, tokenB, true, _factory));
+
+        uint256 decimalsA = 10 ** IERC20Metadata(tokenA).decimals();
+        uint256 decimalsB = 10 ** IERC20Metadata(tokenB).decimals();
+
+        uint256 investment = decimalsA;
+        uint256 out = pool.getAmountOut(investment, tokenA);
+        (uint256 amountA, uint256 amountB, ) = quoteAddLiquidity(tokenA, tokenB, true, _factory, investment, out);
+
+        amountA = (amountA * 1e18) / decimalsA;
+        amountB = (amountB * 1e18) / decimalsB;
+        out = (out * 1e18) / decimalsB;
+        investment = (investment * 1e18) / decimalsA;
+
+        ratio = (((out * 1e18) / investment) * amountA) / amountB;
+
+        return (investment * 1e18) / (ratio + 1e18);/**/
     }
 }
 /*
