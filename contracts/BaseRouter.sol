fbf9d75f9d21ff187a642b9d36f03589d21c4e15
danielalcarraz <dam417@nyu.edu>
2024-10-13 21:57:02 -0400

    add transfers to BaseRouter and remove UniversalQuoter

--- contracts/BaseRouter.sol
+++ contracts/BaseRouter.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.0;
 
 import "@gammaswap/v1-deltaswap/contracts/interfaces/IDeltaSwapPair.sol";
 import "@gammaswap/v1-implementations/contracts/interfaces/external/cpmm/ICPMM.sol";
+import "@gammaswap/v1-periphery/contracts/base/Transfers.sol";
 
 import './libraries/BytesLib2.sol';
 import './libraries/Path2.sol';
 import './interfaces/IProtocolRoute.sol';
 
-abstract contract BaseRouter {
+abstract contract BaseRouter is Transfers {
 
     using BytesLib2 for bytes;
     using Path2 for bytes;
@@ -30,12 +31,9 @@ abstract contract BaseRouter {
         address hop;
     }
 
-    address public immutable WETH;
-
     mapping(uint16 => address) public protocols;
 
-    constructor(address _WETH) {
-        WETH = _WETH;
+    constructor(address _WETH) Transfers(_WETH) {
     }
 
     function addProtocol(uint16 protocolId, address protocol) external virtual {
@@ -51,5 +49,9 @@ abstract contract BaseRouter {
             _path = _path.skipToken();
         }
         tokenOut = _path.skipToken().toAddress(0);
+    }
+
+    function getGammaPoolAddress(address cfmm, uint16 protocolId) internal override virtual view returns(address) {
+        return address(0);
     }
 }
--- contracts/UniversalRouter.sol
+++ contracts/UniversalRouter.sol
@@ -3,9 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@gammaswap/v1-core/contracts/libraries/GammaSwapLibrary.sol";
 import "@gammaswap/v1-periphery/contracts/interfaces/external/IWETH.sol";
-import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
-import './interfaces/IAeroPool.sol';
 import './BaseRouter.sol';
 
 contract UniversalRouter is BaseRouter {
@@ -50,9 +48,7 @@ contract UniversalRouter is BaseRouter {
             (routes[i].pair, dest) = IProtocolRoute(routes[i].hop).getDestination(routes[i].from,
                 routes[i].to, routes[i].fee);
 
-            if(i > 0) {
-                routes[i - 1].dest = dest;
-            }
+            if(i > 0) routes[i - 1].dest = dest;
 
             // decide whether to continue or terminate
             if (hasMultiplePools) {
@@ -91,22 +87,84 @@ contract UniversalRouter is BaseRouter {
         );
     }
 
-    function pay(
-        address token,
-        address payer,
-        address recipient,
-        uint256 value
-    ) internal {
-        if (token == WETH && address(this).balance >= value) {
-            // pay with WETH
-            IWETH(WETH).deposit{value: value}(); // wrap only what is needed to pay
-            IWETH(WETH).transfer(recipient, value);
-        } else if (payer == address(this)) {
-            // pay with tokens already in the contract (for the exact input multihop case)
-            GammaSwapLibrary.safeTransfer(token, recipient, value);
-        } else {
-            // pull payment
-            GammaSwapLibrary.safeTransferFrom(token, payer, recipient, value);
+    function getAmountsOut(uint256 amountIn, bytes memory path) public virtual returns (uint256[] memory amounts, Route[] memory routes) {
+        require(path.length >= 45 && (path.length - 20) % 25 == 0, "INVALID_PATH");
+        routes = new Route[](path.numPools() + 1);
+        amounts = new uint256[](path.numPools() + 1);
+        amounts[0] = amountIn;
+        uint256 i = 0;
+        while (true) {
+            bool hasMultiplePools = path.hasMultiplePools();
+
+            routes[i] = Route({
+                pair: address(0),
+                from: address(0),
+                to: address(0),
+                protocolId: 0,
+                fee: 0,
+                dest: address(0),
+                hop: address(0)
+            });
+
+            // only the first pool in the path is necessary
+            (routes[i].from, routes[i].to, routes[i].protocolId, routes[i].fee) = path.getFirstPool().decodeFirstPool();
+
+            routes[i].hop = protocols[routes[i].protocolId];
+            require(routes[i].hop != address(0), "PROTOCOL_NOT_SET");
+
+            (amounts[i + 1], routes[i].pair, routes[i].fee) = IProtocolRoute(routes[i].hop).getAmountOut(amounts[i],
+                routes[i].from, routes[i].to, routes[i].fee);
+
+            // decide whether to continue or terminate
+            if (hasMultiplePools) {
+                path = path.skipToken();
+            } else {
+                break;
+            }
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    // path is assumed to be reversed from the one in getAmountsOut. In original getAmountsOut it is not reversed
+    function getAmountsIn(uint256 amountOut, bytes memory path) public virtual returns (uint256[] memory amounts, Route[] memory routes) {
+        require(path.length >= 45 && (path.length - 20) % 25 == 0, "INVALID_PATH");
+        routes = new Route[](path.numPools() + 1);
+        amounts = new uint256[](path.numPools() + 1);
+        uint256 i = amounts.length - 1;
+        amounts[i] = amountOut;
+        while (true) {
+            bool hasMultiplePools = path.hasMultiplePools();
+
+            routes[i] = Route({
+                pair: address(0),
+                from: address(0),
+                to: address(0),
+                protocolId: 0,
+                fee: 0,
+                dest: address(0),
+                hop: address(0)
+            });
+
+            // only the first pool in the path is necessary
+            (routes[i].from, routes[i].to, routes[i].protocolId, routes[i].fee) = path.getLastPool().decodeFirstPool();
+
+            routes[i].hop = protocols[routes[i].protocolId];
+            require(routes[i].hop != address(0), "ROUTE_NOT_SET");
+
+            (amounts[i - 1], routes[i].pair, routes[i].fee) = IProtocolRoute(routes[i].hop).getAmountIn(amounts[i],
+                routes[i].from, routes[i].to, routes[i].fee);
+
+            // decide whether to continue or terminate
+            if (hasMultiplePools) {
+                path = path.hopToken();
+            } else {
+                break;
+            }
+            unchecked {
+                --i;
+            }
         }
     }
 }
