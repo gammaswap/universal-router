c27f13f456d8ef07d137551f7416615899254a02
danielalcarraz <dam417@nyu.edu>
2024-10-21 05:56:32 -0400

    cleanup foundry.toml and remove virtual from interfaces

--- contracts/test/IAeroCLPositionManager.sol
+++ contracts/test/IAeroCLPositionManager.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.0;
 
 interface IAeroCLPositionManager {
-    function setOwner(address _owner) external virtual;
+    function setOwner(address _owner) external;
 }
--- contracts/test/ICLGaugeFactory.sol
+++ contracts/test/ICLGaugeFactory.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.0;
 
 interface ICLGaugeFactory {
-    function setNonfungiblePositionManager(address _nonfungiblePositionManager) external virtual;
-    function setNotifyAdmin(address _notifyAdmin) external virtual;
+    function setNonfungiblePositionManager(address _nonfungiblePositionManager) external;
+    function setNotifyAdmin(address _notifyAdmin) external;
 }
